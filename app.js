ae8875da8e6d265d0c6b6815abb7e1d00ba26e29
Anne Chen <annechen.anne@gmail.com>
2018-05-05 10:38:07 -0700

    modified files

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 ///Foursquare Client ID and Client Secret
 const VENUE_DETAIL_URL =  "https://api.foursquare.com/v2/venues/";
 const VENUE_SEARCH_URL = "https://api.foursquare.com/v2/venues/search";
-const VENUE_PHOTOS_URL = "https://api.foursquare.com/v2/venues/VENUE_ID/photos";
 const CLIENT_ID =
  "F5351GDXA2MHDHXSVNL3IFSHCULZXYQHVEYTSA2WY3AQQIOO";
 const CLIENT_SECRET=
@@ -39,7 +38,7 @@ function getPhotosAPIResults( venue_id, callback ){
     return getAPI(url,query, callback );
 }
 
-function addPhotoResultToVenue(venue_id, photo){
+function addPhotoResultToVenue(venue, photo){
     
     const currentPhoto = {
         "prefix": "https://igx.4sqi.net/img/general/",
@@ -48,40 +47,32 @@ function addPhotoResultToVenue(venue_id, photo){
         "height": 200,      
     };
     //const someImageURL = photo.prefix+photo.width+"x"+photo.height+photo.suffix;
-    const someImageURL = photo.prefix+currentPhoto.width+currentPhoto.height+photo.suffix;
+    const someImageURL = currentPhoto.prefix+currentPhoto.width+currentPhoto.height+currentPhoto.suffix;
  
-    console.log('generated photo URL:', someImageURL);
-  
+    //console.log('generated photo URL:', someImageURL);
+    altText = "no photo for ${venue.name}";
+    if(photo){
+        altText =`${venue.name}-photos`;    
+    }
     // element with class photo is descendant the of element with class venue.
-      
-   $(`.venue[data-venue-id="${venue_id}"] .photo`).attr('src', someImageURL).attr("alt", "venue-picture");
-    
-//    let container = 0;
-//$("img.photo").each(function() {console.log($(this).height())})
-//$("img.photo").each(function() {container=Math.max(container, $(this).height())});
-//$(".restaurant-info").each(function() {container=Math.max(container, $(this).height())});
-//$(".restaurant-info").css("height", container+"px");  
-//$("img.photo").each(function() {container=Math.max(container, $(this).height())});
-//$("img.photo").css("height", container+"px");      
-//    
-    
-    
+    $(`.venue[data-venue-id="${venue.id}"] .photo`)
+    .attr("src", someImageURL)
+    .attr("alt", "altText");
 }
+
 function renderResult(venue) {
    
-    return (
-        `
+    return (`
             <div class = "col-4">
                 <div class = "restaurant-info">    
                     <div class="venue" data-venue-id="${venue.id}">
                         <div class = "wrapper">
-                            <a href = "https://foursquare.com/v/${venue.name}/${venue.id}"><h4>${venue.name}</h4></a>
+                            <a href = "https://foursquare.com/v/${venue.name}/${venue.id}" target="_blank"><h4>${venue.name}</h4></a>
                         </div>
-                        <a href = "https://foursquare.com/v/${venue.name}/${venue.id}"><img class="photo"></a>
+                        <a href = "https://foursquare.com/v/${venue.name}/${venue.id}" target="_blank"><img class="photo" src = "images/hourglass.gif" alt = "waiting for photo"></a>
                     </div>
                 </div>
-            <div>
-        `
+            <div>`
     );
     
 
@@ -89,19 +80,19 @@ function renderResult(venue) {
 }
 
 function watchSubmit() {
-    $('.form').submit(event => {
+    $(".form").submit(event => {
         event.preventDefault();
-        const queryTarget = $(event.currentTarget).find('.search-restaurant');
+        const queryTarget = $(event.currentTarget).find(".search-restaurant");
         const searchTerm = queryTarget.val().trim();
         const query = {
             near: "Taiwan",
             query: searchTerm,
             categoryId: "4d4b7105d754a06374d81259",
         }
-    // clear out the input
+        // clear out the input
         queryTarget.val("");
         getAPI(VENUE_SEARCH_URL, query, function(data){
-            console.log(data);  
+            //console.log(data);  
             
             
             if (data.response.venues.length == 0){
@@ -110,8 +101,8 @@ function watchSubmit() {
                 
             }      
             const results = data.response.venues.map((venue, index) =>{
-                let venue_id = venue.id;   
-                console.log("venue_id",venue_id, venue);
+            let venue_id = venue.id;   
+                
                 getPhotosAPIResults( venue_id, function(photoData) {
                     
                     if( photoData.response.photos.count > 0 ){
@@ -120,23 +111,22 @@ function watchSubmit() {
                        
                         // this will run last, after the *** below
                      
-                        addPhotoResultToVenue(venue.id, photo);
+                        addPhotoResultToVenue(venue, photo);
                      
-                    // we have found a photo. find the venue already on the page and add the photo to it
+                        // we have found a photo. find the venue already on the page and add the photo to it
                   
                     }
                     else if( photoData.response.photos.count == 0) {
                                   
-//                        
-//                      let photo = $("img").css("background-image", "url(images/default_picture.png)");
-                        
-                        let photo = $(`.venue[data-venue-id="${venue_id}"] .photo`).attr("src", "images/LogoMakr.png").attr("alt", "default-picture");
+                        let photo = $(`.venue[data-venue-id="${venue_id}"] .photo`)
+                        .attr("src", "images/LogoMakr.png")
+                        .attr("alt", "default-picture");
                     }
                 });
                 return renderResult(venue);
             });
         
-            $('section.result >.row').html(results);       
+            $("section.result >.row").html(results);       
             displayResults();
         });
     
@@ -148,48 +138,44 @@ function noResults(){
    
     $(".no-result").removeAttr("hidden"); 
     $(".no-result").show(); 
-    $(".no-result").html(
-     `
+    $(".no-result").html(`
         <div class = "row">
-
             <div class = "col-12">    
-
                 <h1 class = "no-data">Sorry, no result. Try again.</h1>
-
             </div>
-
-        </div>
-    `
+        </div>`
     );
     
     
 }
 
 function watchNewSearchButton(){
-  $('.new-search-button-container button').click(function(event){
+  $(".new-search-button-container button").click(function(event){
     // hide results, show new search, focus search box
     event.preventDefault();
     displaySearch();
     $('input[type="search"]').focus();
+    $("footer").show();  
   });
 }
 // triggered on "search" button
 function displayResults(){
      
     $(".new-search-button-container").show();    
-    $('section.result').show();
-    $('section.search').hide();
-    $('body').addClass('result');
-    $('body').removeClass('search');
+    $("section.result").show();
+    $("section.search").hide();
+    $("footer").hide();
+    $("body").addClass('result');
+    $("body").removeClass('search');
 }
 // triggered on "search again"
 function displaySearch(){
     $("div.no-result").hide();    
     $(".new-search-button-container").hide();    
-    $('section.result').hide();
-    $('section.search').show();
-    $('body').removeClass('result');
-    $('body').addClass('search');
+    $("section.result").hide();
+    $("section.search").show();
+    $("body").removeClass("result");
+    $("body").addClass("search");
 }
 
 $(watchNewSearchButton);
